3859ce509600b7c7c8a87cf3a5646535930c56f4
er.vinit@gmail.com <er.vinit@gmail.com>
2023-04-04 19:55:31 -0400

    remove handler

--- src/main/java/com/learning/demo/controller/CustomCRUDController.java
+++ src/main/java/com/learning/demo/controller/CustomCRUDController.java
@@ -6,6 +6,7 @@ import com.learning.demo.model.Location;
 import com.learning.demo.service.LocationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,11 +20,13 @@ public class CustomCRUDController {
     LocationService locationService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Location> findAllLocation() {
         return locationService.findAll();
     }
 
-    @PostMapping("/add")
+    @PostMapping(value = "/add")
+    @ResponseStatus(HttpStatus.CREATED)
     public List<Location> addLocation(@RequestBody @Valid Location location) throws DuplicateLocationException {
         Location loc = locationService.createLocation(location);
         log.info("Record Created :  {}", loc);
@@ -31,6 +34,7 @@ public class CustomCRUDController {
     }
 
     @PutMapping("/update")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public List<Location> updateLocation(@RequestBody @Valid Location location) throws LocationNotFoundException {
         Location loc = locationService.updateLocation(location);
         log.info("Record Updated :  {}", loc);
@@ -38,6 +42,7 @@ public class CustomCRUDController {
     }
 
     @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public List<Location> deleteLocation(@PathVariable Integer id) throws LocationNotFoundException {
         locationService.deleteById(id);
         log.info("Record Deleted :  {}", id);
